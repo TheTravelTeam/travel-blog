68445216051f568197b01ab9df0010155746a26b
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2025-04-17 17:33:12 +0200

    chore(KAN-138): configure ESLint, Prettier, Husky, and dotenv for the project

---
+++ frontend/eslint.config.js
@@ -0,0 +1,62 @@
+import angularEslintPlugin from '@angular-eslint/eslint-plugin';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import angularTemplatePlugin from '@angular-eslint/eslint-plugin-template';
+import angularTemplateParser from '@angular-eslint/template-parser';
+
+export default [
+  {
+    ignores: ['projects/**/*', 'node_modules', 'dist', '**/*.spec.ts'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        createDefaultProgram: true,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': tsEslintPlugin,
+      '@angular-eslint': angularEslintPlugin,
+    },
+    rules: {
+      ...tsEslintPlugin.configs.recommended.rules,
+      ...tsEslintPlugin.configs.stylistic.rules,
+      ...angularEslintPlugin.configs.recommended.rules,
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
+      ],
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error', 'info'],
+        },
+      ],
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      '@angular-eslint/template': angularTemplatePlugin,
+    },
+    rules: {
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/eqeqeq': 'error',
+    },
+  },
+];
