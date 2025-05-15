c6316fa8d53f01fcc382e7b42e9d3ed922dd7298
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-05-15 11:02:21 +0200

    Feat/kan 137/create.btn.component (#4)
    * feat(KAN-137): create btn component, btn type and color,size, radius types + Create homepage page
    
    * feat(KAN-137): create btn component, add variants and style scss

--- src/app/components/Button/button/button.component.ts
+++ src/app/components/Button/button/button.component.ts
@@ -1,15 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ButtonService } from '../button.service';
-
+import { CommonModule } from '@angular/common';
+import { Btn, btnDefault } from '../../../model/btn';
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   private buttonService: ButtonService = inject(ButtonService);
+
+  @Input() color: Btn['color'] = btnDefault['color'];
+  @Input() radius: Btn['radius'] = btnDefault['radius'];
+  @Input() size: Btn['size'] = btnDefault['size'];
+  @Input() isDisabled: Btn['isDisabled'] = btnDefault['isDisabled'];
+  @Input() text: Btn['text'] = btnDefault['text'];
+  @Input() startIcon: Btn['startIcon'] = btnDefault['startIcon'];
+  @Input() endIcon: Btn['endIcon'] = btnDefault['endIcon'];
+  @Input() isActionBtn: Btn['isActionBtn'] = btnDefault['isActionBtn'];
+  @Input() type: Btn['type'] = btnDefault['type'];
+  @Input() icon: Btn['icon'] = btnDefault['icon'];
 
   onClick() {
     this.buttonService.clickEvent();
---
+++ src/app/model/variant.ts
@@ -0,0 +1,10 @@
+export type Color = 'primary' | 'secondary' | 'danger';
+export type Radius = 'border_sm' | 'border_md' | 'border_lg';
+export type Size = 'sm' | 'md' | 'lg';
+
+// type Variant = 'solid';
+// type Size =
+// type StartContent =
+// type EndContent =
+// type IsDisabled =
+// type IsIconOnly =
