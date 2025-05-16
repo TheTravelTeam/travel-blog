4f12dc0bd2dadeee88675e679eef5fc999d26006
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-05-16 15:50:26 +0200

    feat(KAN-171): Create link component (#5)
    * feat(KAN-171): Create link component, scss, model
    
    * feat(KAN-171) modify link interface into type
    
    * feat(KAN-171) update interface into type and update requiered/optionnal props
    
    * feat(KAN-171): delete btnComponent call and import in the Homepage

---
+++ src/app/model/link.ts
@@ -0,0 +1,29 @@
+import { Color } from './variant';
+
+export type Link = {
+  label: string;
+  route: string;
+  color?: Color;
+  icon?:
+    | 'add'
+    | 'close'
+    | 'arrow_back'
+    | 'remove'
+    | 'chevron_right'
+    | 'chevron_left'
+    | 'keyboard_arrow_down'
+    | 'keyboard_arrow_up'
+    | 'search'
+    | 'hiking'
+    | 'favorite'
+    | 'account_circle'
+    | 'auto_stories'
+    | 'map_search'
+    | 'travel_explore'
+    | 'chat_bubble';
+};
+
+export const linkDefault: Required<Omit<Link, 'label' | 'route'>> = {
+  color: 'white',
+  icon: 'hiking',
+};
