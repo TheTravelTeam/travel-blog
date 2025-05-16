362e5bf3611e819deaa3dfe572d90201d9f82aba
Antoine Peral <peral.antoine@gmail.com>
2025-05-16 14:33:14 +0200

    feat(KAN-171): fix after review

--- src/app/components/link/link/link.component.ts
+++ src/app/components/link/link/link.component.ts
@@ -11,8 +11,8 @@ import { RouterModule } from '@angular/router';
   styleUrl: './link.component.scss',
 })
 export class LinkComponent {
-  @Input() label!: Link['label'];
-  @Input() route!: Link['route'];
+  @Input({ required: true }) label!: Link['label'];
+  @Input({ required: true }) route!: Link['route'];
   @Input() color: Link['color'] = linkDefault['color'];
   @Input() icon: Link['icon'] = linkDefault['icon'];
 }
