a2111649a81c3e4ebfe4d6e75a68f226e391dddd
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-06-10 16:33:26 +0200

    feat(KAN-170): create divider component, scss, model and service (#8)
    * feat(KAN-170): create divider component, scss, model and service
    
    * feat(KAN-170): delete btnComponent call and import in the Homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ToggleBtnComponent } from '../../components/toggle-btn/toggle-btn.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ToggleBtnComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
