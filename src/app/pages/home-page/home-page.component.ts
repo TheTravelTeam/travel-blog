8ba6cb25ce344f4bf72f61661e8302dd58ba5d93
Antoine Peral <peral.antoine@gmail.com>
2025-06-10 15:16:47 +0200

    feat(KAN-164): fix after review

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ToggleBtnComponent } from '../../components/toggle-btn/toggle-btn.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ToggleBtnComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
