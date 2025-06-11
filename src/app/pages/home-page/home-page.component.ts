6cb8662b89339a9d22dc28982b116d4660dee09c
Antoine Peral <peral.antoine@gmail.com>
2025-06-11 10:41:14 +0200

    fix: btn css

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/Button/button/button.component';
+import { IconComponent } from '../../components/icon/icon.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, IconComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
