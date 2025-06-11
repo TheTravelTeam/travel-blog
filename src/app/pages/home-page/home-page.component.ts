5566488a5f60f38c3b97e3110de149b727f16f9c
Antoine Peral <peral.antoine@gmail.com>
2025-06-11 10:14:10 +0200

    feat(KAN-160): fix after review

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { CheckboxComponent } from '../../components/checkbox/checkbox/checkbox.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CheckboxComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
