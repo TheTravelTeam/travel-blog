326a574ec4b398403d699818e699b6aba077f768
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-06-11 10:50:51 +0200

    feat(KAN-160): create checkbox component  (#6)
    * feat(KAN-160): create component checkbox, scss, model
    
    * feat(KAN-160) modify interface into type
    
    * feat(KAN-160) update inteface into type and add label
    
    * feat(KAN-160): delete btnComponent call and import in the Homepage
    
    * feat(KAN-160): checkbox component
    
    * feat(KAN-160): fix after review

---
+++ src/app/components/checkbox/checkbox.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Checkbox, checkboxDefault } from '../../../model/checkbox';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+})
+export class CheckboxComponent {
+  @Input({ required: true }) checkboxName!: Checkbox['checkboxName'];
+  @Input() label?: Checkbox['label'];
+  @Input() icon: Checkbox['icon'] = checkboxDefault['icon'];
+  @Input() isDisabled: Checkbox['isDisabled'] = checkboxDefault['isDisabled'];
+}
