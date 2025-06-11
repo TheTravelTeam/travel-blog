2ff2b8f60209b09019a9eeb9e4e80ae974455f97
Antoine Peral <peral.antoine@gmail.com>
2025-06-11 10:44:38 +0200

    fix: btn css and reset css

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../components/Button/button/button.component';
-import { IconComponent } from '../../components/icon/icon.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ButtonComponent, IconComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
