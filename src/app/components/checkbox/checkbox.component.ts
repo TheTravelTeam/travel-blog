a4abb91d8dad50106cb1e959ef2c42c7500cd2bc
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-06-12 14:14:44 +0200

    fix: delete homepage component and standalone (#15)
    * fix: delete homepage component and standalone
    
    * fix: delete import on homepage

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { Checkbox, checkboxDefault } from '../../../model/checkbox';
 import { CommonModule } from '@angular/common';
+import { Checkbox, checkboxDefault } from '../../model/checkbox';
 
 @Component({
   selector: 'app-checkbox',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.scss',
