25a0516768b3d217a0b01bf55b83e265affc95c0
Antoine Peral <peral.antoine@gmail.com>
2025-06-12 14:08:29 +0200

    fix: delete import on homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../components/Button/button/button.component';
-import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
-import { ToggleBtnComponent } from '../../components/toggle-btn/toggle-btn.component';
 @Component({
   selector: 'app-home-page',
-  imports: [ButtonComponent, CheckboxComponent, ToggleBtnComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
