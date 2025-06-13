5369b901763145c598279cf42b2c8f776ad64a68
AntoP <117115061+AntoinePeral@users.noreply.github.com>
2025-06-13 14:28:13 +0200

    feat(KAN-169): Create breacrumb component
    - create breadcrumb component models, scss
    - update icon size

---
+++ src/app/model/icon.ts
@@ -0,0 +1,61 @@
+import { IconColor, IconSize } from './variant';
+
+export const iconDefault: Required<Omit<Icon, 'name'>> = {
+  size: 'sm',
+  color: null,
+  weight: 400,
+  fill: false,
+  grade: 0,
+  customClass: '',
+};
+
+export type Icon = {
+  name: IconName;
+  size?: IconSize;
+  color?: IconColor | null;
+  weight?: IconWeight;
+  fill?: boolean;
+  grade?: IconGrade;
+  customClass?: string;
+};
+
+export type IconName =
+  | 'add'
+  | 'add_box'
+  | 'close'
+  | 'arrow_back'
+  | 'remove'
+  | 'chevron_right'
+  | 'chevron_left'
+  | 'keyboard_arrow_down'
+  | 'keyboard_arrow_up'
+  | 'search'
+  | 'hiking'
+  | 'favorite'
+  | 'chat_bubble'
+  | 'help'
+  | 'info'
+  | 'warning'
+  | 'error'
+  | 'check'
+  | 'check_circle'
+  | 'star'
+  | 'delete'
+  | 'edit_square'
+  | 'edit'
+  | 'visibility'
+  | 'visibility_off'
+  | 'import_contacts'
+  | 'globe_location_pin'
+  | 'language'
+  | 'travel_explore'
+  | 'map_search'
+  | 'calendar_today'
+  | 'location_on'
+  | 'check_box_outline_blank'
+  | 'disabled_by_default'
+  | 'home'
+  | 'account_circle';
+
+export type IconWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700;
+export type IconGrade = -25 | 0 | 200;
