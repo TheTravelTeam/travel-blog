1d306e7bf3ba5e61cc0c4310f37a180bc609511b
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-07-08 15:42:47 +0200

    feat(KAN-202):card ok with alert event in waiting route

---
+++ src/app/card-ready-to-use/visual-trip-card/visual-trip-card.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VisualTripCardComponent } from './visual-trip-card.component';
+
+describe('VisualTripCardComponent', () => {
+  let component: VisualTripCardComponent;
+  let fixture: ComponentFixture<VisualTripCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VisualTripCardComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VisualTripCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
