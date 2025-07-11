865e02a6151479cd7f4690fcf909600e95fb1184
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-07-11 14:06:17 +0200

    feat(KAN-209):missing some icons and some image effects

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,21 @@
 import { FooterComponent } from './../../components/footer/footer.component';
 import { Component } from '@angular/core';
+import { TopBarComponent } from '../../components/top-bar/top-bar.component';
+import { VisualTripCardComponent } from '../../components/card-ready-to-use/visual-trip-card/visual-trip-card.component';
+import { CardComponent } from '../../components/card/card.component';
+import { ButtonComponent } from '../../components/Button/button/button.component';
+import { ChipComponent } from '../../components/Atoms/chip/chip.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [
+    FooterComponent,
+    TopBarComponent,
+    VisualTripCardComponent,
+    CardComponent,
+    ButtonComponent,
+    ChipComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
