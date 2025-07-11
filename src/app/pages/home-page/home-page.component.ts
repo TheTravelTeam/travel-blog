98953e320eade51950b6850704365608d7343e71
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-07-11 14:06:17 +0200

    feat(KAN-209):missing some icons and some image effects

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,19 +5,28 @@ import { VisualTripCardComponent } from '../../components/card-ready-to-use/visu
 import { CardComponent } from '../../components/card/card.component';
 import { ButtonComponent } from '../../components/Button/button/button.component';
 import { ChipComponent } from '../../components/Atoms/chip/chip.component';
+<<<<<<< HEAD
 import { IconComponent } from '../../components/icon/icon.component';
 
+=======
+>>>>>>> da51415 (feat(KAN-209):missing some icons and some image effects)
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
+<<<<<<< HEAD
     FooterComponent,
+=======
+>>>>>>> da51415 (feat(KAN-209):missing some icons and some image effects)
     TopBarComponent,
     VisualTripCardComponent,
     CardComponent,
     ButtonComponent,
     ChipComponent,
+<<<<<<< HEAD
     IconComponent,
+=======
+>>>>>>> da51415 (feat(KAN-209):missing some icons and some image effects)
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
