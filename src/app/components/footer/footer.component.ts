8f013bd7a2073ba68570a7ffea7459260a3fd13d
Antoine Peral <peral.antoine@gmail.com>
2025-07-16 11:41:32 +0200

    feat(KAN-154): make footer responsive with Scss, media queries, and signals

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { BreakpointService } from './../../services/breakpoint.service';
+import { Component, computed, inject } from '@angular/core';
 import { LogoComponent } from '../Atoms/logo/logo.component';
 import { LinkComponent } from '../link/link/link.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [LogoComponent, LinkComponent],
+  imports: [LogoComponent, LinkComponent, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  @Input() isMobile = false;
+  private breakpointService = inject(BreakpointService);
+
+  isTabletOrMobile = this.breakpointService.isMobileOrTablet;
+
+  colorSignal = computed(() => (this.breakpointService.isMobileOrTablet() ? 'white' : 'primary'));
 }
