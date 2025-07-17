fdd97da3f98f1c5c0cd948ba9c292b2b44b90108
Antoine Peral <peral.antoine@gmail.com>
2025-07-17 12:28:00 +0200

    feat(KAN-203): add scroll sticky and some css rules

--- src/app/model/divider.ts
+++ src/app/model/divider.ts
@@ -3,6 +3,7 @@ export type DividerColor = 'default' | 'primary' | 'secondary' | 'danger' | 'whi
 export type DividerThickness = 'thin' | 'medium' | 'thick';
 export type DividerMargin = 'sm' | 'md' | 'lg';
 export type DividerRadius = '5' | '10' | '15' | '';
+export type DividerSize = '30' | '50' | '70' | '100';
 
 export type Divider = {
   orientation?: DividerOrientation;
@@ -11,6 +12,7 @@ export type Divider = {
   margin?: DividerMargin;
   isOpenMobile: boolean;
   radius: DividerRadius;
+  size: DividerSize;
 };
 
 export const dividerDefault: Divider = {
@@ -20,4 +22,5 @@ export const dividerDefault: Divider = {
   margin: 'sm',
   isOpenMobile: false,
   radius: '',
+  size: '50',
 };
