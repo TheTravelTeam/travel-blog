7aaa45421be1a37f0544692862356ff530a19b25
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-08-26 14:50:06 +0200

    feat(KAN-209):last commit with full corrections

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,18 +1,16 @@
+import { VisualTripCardComponent } from 'components/Molecules/Card-ready-to-use/visual-trip-card/visual-trip-card.component';
 import { FooterComponent } from './../../components/footer/footer.component';
 import { Component } from '@angular/core';
-import { TopBarComponent } from '../../components/top-bar/top-bar.component';
-import { VisualTripCardComponent } from '../../components/card-ready-to-use/visual-trip-card/visual-trip-card.component';
-import { CardComponent } from '../../components/card/card.component';
-import { ButtonComponent } from '../../components/Button/button/button.component';
-import { ChipComponent } from '../../components/Atoms/chip/chip.component';
-import { IconComponent } from '../../components/icon/icon.component';
+import { CardComponent } from 'components/Atoms/Card/card.component';
+import { ButtonComponent } from 'components/Atoms/Button/button.component';
+import { ChipComponent } from 'components/Atoms/chip/chip.component';
+import { IconComponent } from 'components/Atoms/Icon/icon.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     FooterComponent,
-    TopBarComponent,
     VisualTripCardComponent,
     CardComponent,
     ButtonComponent,
