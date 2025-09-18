67b0b8281215828018ae7f0d506258494787f123
antop <antoine.peral@rossel.fr>
2025-09-18 21:42:45 +0200

    feat(kan-266): Add test on auth service

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),          // fournit HttpClient
+        provideHttpClientTesting(),   // remplace le backend par un mock
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
